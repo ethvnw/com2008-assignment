ce860309a6f9fe96c434d980b0f32d3fc0507134
Vivek Choradia <63441726+VChoradia@users.noreply.github.com>
2022-10-31 15:40:06 +0000

    Initial setup

---
+++ src/assignment/DBDriver.java
@@ -0,0 +1,24 @@
+package assignment;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBDriver {
+    static String URL = "jdbc:mysql://stusql.dcs.shef.ac.uk/";
+    static String DBNAME = "team001";
+    static String USER = "team001";
+    static String PASSWORD = "3314b4b3";
+
+    public static void main(String[] args) {
+        System.out.println("Connecting...");
+
+        try {
+            Connection con = DriverManager.getConnection(URL+DBNAME, USER, PASSWORD);
+            con.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+}
