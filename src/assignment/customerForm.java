2d442700a211e95fcd40c5f7d698214ddc27a722
RayHan0930 <whan5@sheffield.ac.uk>
2022-11-13 20:58:10 +0000

    Customer Details Page 2.0

--- src/assignment/customerForm.java
+++ src/assignment/customerForm.java
@@ -1,44 +1,84 @@
 package assignment;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class customerForm {
-    public static void main(String args[]){
+public class customerForm extends JFrame implements ActionListener {
+    private JTextField tfForename, tfSurname, tfHouseNo, tfRoadName, tfCityName, tfPostcode;
+
+    public customerForm (){
         //Creating the Frame
         JFrame frame = new JFrame("Customer Details");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(1500, 800);
-
-        JComponent one = new JLabel("Left Split");
-        one.setBorder(BorderFactory.createLineBorder(Color.MAGENTA));
-        JComponent two = new JLabel("Right Split");
-        two.setBorder(BorderFactory.createLineBorder(Color.ORANGE));
-        JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, one, two);
-        frame.add(splitPane);
 
         //Creating the top panel
         JPanel panelTop = new JPanel(); // the panel is not visible in output
         JLabel labelTop = new JLabel("Build - a Bike");
         panelTop.add(labelTop); // Components Added using Flow Layout
 
-        //Creating the panel at bottom and adding components
-        JPanel panel = new JPanel(); // the panel is not visible in output
-        JLabel label = new JLabel("Enter Text");
-        JTextField tf = new JTextField(10); // accepts upto 10 characters
-        JButton send = new JButton("Send");
-        JButton reset = new JButton("Reset");
-        panel.add(label); // Components Added using Flow Layout
-        panel.add(tf);
-        panel.add(send);
-        panel.add(reset);
+        //Creating the middle panel (Left)
+        JPanel panelLeft = new JPanel(); // the panel is not visible in output
+        JLabel labelLeft = new JLabel("Customer Details");
+        //panelLeft.add(labelLeft); // Components Added using Flow Layout
+        panelLeft.setBorder(BorderFactory.createLineBorder(Color.BLACK));
 
-        // Text Area at the Center
-        //JTextArea ta = new JTextArea();
+        JPanel labels = new JPanel(new GridLayout(0,1));
+        JPanel controls = new JPanel(new GridLayout(0,1));
+        panelLeft.add(labels, BorderLayout.WEST);
+        panelLeft.add(controls, BorderLayout.CENTER);
+
+        tfForename = new JTextField(10); // accepts upto 10 characters
+        tfSurname = new JTextField(10); // accepts upto 10 characters
+        tfHouseNo = new JTextField(10); // accepts upto 10 characters
+        tfRoadName = new JTextField(10); // accepts upto 10 characters
+        tfCityName = new JTextField(10); // accepts upto 10 characters
+        tfPostcode = new JTextField(10); // accepts upto 10 characters
+
+
+        labels.add(new JLabel("Forename: ", SwingConstants.LEFT));
+        controls.add(tfForename);
+        labels.add(new JLabel("Surname: ", SwingConstants.LEFT));
+        controls.add(tfSurname);
+        labels.add(new JLabel("House No: ", SwingConstants.LEFT));
+        controls.add(tfHouseNo);
+        labels.add(new JLabel("Road Name: ", SwingConstants.LEFT));
+        controls.add(tfRoadName);
+        labels.add(new JLabel("City Name: ", SwingConstants.LEFT));
+        controls.add(tfCityName);
+        labels.add(new JLabel("Postcode: ", SwingConstants.LEFT));
+        controls.add(tfPostcode);
+
+        //Creating the middle panel (Right)
+        JPanel panelRight = new JPanel(); // the panel is not visible in output
+        JLabel labelTwo = new JLabel("Order");
+        panelRight.add(labelTwo); // Components Added using Flow Layout
+        panelRight.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+
+        //Split middle panel into two
+        JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, panelLeft, panelRight);
+        splitPane.setDividerLocation(710);
+
+        //Creating the panel at bottom and adding components
+        JPanel panelBottom = new JPanel(); // the panel is not visible in output
+        JButton proceedToPayment = new JButton("Proceed to Payment");
+        panelBottom.add(proceedToPayment);
+        proceedToPayment.addActionListener(this);
 
         //Adding Components to the frame.
-        frame.getContentPane().add(BorderLayout.SOUTH, panel);
+        frame.getContentPane().add(BorderLayout.SOUTH, panelBottom);
         frame.getContentPane().add(BorderLayout.NORTH, panelTop);
-        //frame.getContentPane().add(BorderLayout.CENTER, ta);
-        frame.setVisible(true);
+        frame.getContentPane().add(BorderLayout.CENTER, splitPane);
+
+    }
+
+    public static void main(String[] args) {
+        new customerForm().setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
     }
 }
