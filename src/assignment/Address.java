c5cf1861fd5cd8428682f9bac0ded75add273273
Vivek Choradia <63441726+VChoradia@users.noreply.github.com>
2022-11-13 20:39:23 +0000

    Setup new functions

--- src/assignment/Address.java
+++ src/assignment/Address.java
@@ -1,7 +1,6 @@
 package assignment;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Address {
 
@@ -19,27 +18,41 @@ public class Address {
 
 
 
-    public void createAddress() {
-        String query = "INSERT INTO address" +
-                        "VALUES("+houseNum + ", "+ road + ", "+ city + ", "+ postcode + ");";
+    public void createAddress() throws SQLException {
 
-        DBDriver.processInsertQuery(query);
+        Address newAdd = findAddress(houseNum, postcode);
+        if (newAdd == null) {
+            String query = "INSERT INTO address(houseNum, road, city, postcode) " +
+                    "VALUES(\"" + houseNum + "\", \"" + road + "\", \"" + city + "\", \"" + postcode + "\");";
+            DBDriver.processInsertQuery(query);
+        }
     }
 
     public static Address findAddress(String houseNum, String postcode) throws SQLException {
 
-        String query = "SELECT FROM address WHERE houseNum  = " + houseNum + "AND postcode = " + postcode + ";";
-        ResultSet res = DBDriver.processGetOutput(query);
+        try (Connection con = DriverManager.getConnection(DBDriver.URL + DBDriver.DBNAME, DBDriver.USER, DBDriver.PASSWORD)) {
 
-        Address add;
+            Statement stmt = con.createStatement();
 
-        if (res != null) {
-            add = new Address(res.getString("houseNum"),
-                    res.getString("road"),
-                    res.getString("city"),
-                    res.getString("postcode"));
-            return add;
+            String query = "SELECT * FROM address WHERE houseNum  = \"" + houseNum + "\" AND postcode = \"" + postcode + "\";";
+
+            ResultSet res = stmt.executeQuery(query);
+
+            Address add;
+
+            while (res.next()) {
+                add = new Address(res.getString("houseNum"),
+                        res.getString("road"),
+                        res.getString("city"),
+                        res.getString("postcode"));
+                return add;
+            }
+
+            res.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
         }
+
 
         return null;
     }
