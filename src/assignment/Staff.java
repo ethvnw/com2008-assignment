9e01a5c3446911f2079689f2b6a8ab8373a5a6bb
Vivek Choradia <63441726+VChoradia@users.noreply.github.com>
2022-11-14 10:34:15 +0000

    added login()

--- src/assignment/Staff.java
+++ src/assignment/Staff.java
@@ -2,6 +2,7 @@ package assignment;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.Signature;
+import java.sql.*;
 
 import javax.crypto.Cipher;
 
@@ -21,5 +22,29 @@ public class Staff {
         this.staffId = staffId;
         this.username = username;
         this.password = password;
+    }
+
+    public int login() throws Exception, SQLException {
+
+        AES_ENCRYPTION aes_encryption = new AES_ENCRYPTION();
+        aes_encryption.init();
+
+        String query = "SELECT * FROM staff WHERE username = \"" + this.username +
+                        "\" AND password = \"" + aes_encryption.encrypt(password) +"\";";
+
+        try (Connection con = DriverManager.getConnection(DBDriver.URL + DBDriver.DBNAME, DBDriver.USER, DBDriver.PASSWORD)) {
+
+            Statement stmt = con.createStatement();
+            ResultSet res = stmt.executeQuery(query);
+
+            while(res.next()) {
+                return res.getInt("staffId");
+            }
+
+            return 0;
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return 0;
     }
 }
