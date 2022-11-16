03ca94eb712963cb041e137be560d60fd6395a11
Vivek Choradia <63441726+VChoradia@users.noreply.github.com>
2022-11-16 21:09:57 +0000

    Completed TODO tasks

--- src/assignment/models/Encryption.java
+++ src/assignment/models/Encryption.java
@@ -3,16 +3,9 @@ package assignment.models;
 /** To process Encryption/Decryption for staff login.
  * @author Vivek V Choradia
  * @version 1.0
- * @lastUpdated 14-11-2022 10:37
+ * @lastUpdated 16-11-2022 20:22
  */
 
-// TODO Update decryption (make it work)
-
-
-///////////////////////////////////////
-//CHANGE THIS FILE -- COPIED FROM THE INTERNET
-//https://www.section.io/engineering-education/implementing-aes-encryption-and-decryption-in-java/
-///////////////////////////////////////
 
 import java.util.Base64;
 import java.security.spec.KeySpec;
@@ -23,35 +16,47 @@ import javax.crypto.spec.DESedeKeySpec;
 
 public class Encryption {
     private static final String UNICODE_FORMAT = "UTF8";
-    public static final String DESEDE_ENCRYPTION_SCHEME = "DESede";
+    public static final String ENCRYPTION_SCHEME = "DESede";
     private KeySpec ks;
     private SecretKeyFactory skf;
     private Cipher cipher;
     byte[] arrayBytes;
-    private String myEncryptionKey;
-    private String myEncryptionScheme;
+    private String EncryptionKey;
+    private String EncryptionScheme;
     SecretKey key;
     public Encryption() throws Exception {
-        myEncryptionKey = "ThisIsSpartaThisIsSparta";
-        myEncryptionScheme = DESEDE_ENCRYPTION_SCHEME;
-        arrayBytes = myEncryptionKey.getBytes(UNICODE_FORMAT);
+        EncryptionKey = "TEAM001";
+        EncryptionScheme = ENCRYPTION_SCHEME;
+        arrayBytes = EncryptionKey.getBytes(UNICODE_FORMAT);
         ks = new DESedeKeySpec(arrayBytes);
-        skf = SecretKeyFactory.getInstance(myEncryptionScheme);
-        cipher = Cipher.getInstance(myEncryptionScheme);
+        skf = SecretKeyFactory.getInstance(EncryptionScheme);
+        cipher = Cipher.getInstance(EncryptionScheme);
         key = skf.generateSecret(ks);
     }
+
+    /**
+     * To encrypt the given password
+     * @param unencryptedString password
+     * @return encrypted password
+     */
     public String encrypt(String unencryptedString) {
         String encryptedString = null;
         try {
             cipher.init(Cipher.ENCRYPT_MODE, key);
             byte[] plainText = unencryptedString.getBytes(UNICODE_FORMAT);
             byte[] encryptedText = cipher.doFinal(plainText);
-            encryptedString = new String(Base64.getEncoder().encodeToString(encryptedText));
+            encryptedString = Base64.getEncoder().encodeToString(encryptedText);
         } catch (Exception e) {
             e.printStackTrace();
         }
         return encryptedString;
     }
+
+    /**
+     * To decrypt the password
+     * @param encryptedString password
+     * @return decrypted password
+     */
     public String decrypt(String encryptedString) {
         String decryptedText=null;
         try {
@@ -65,44 +70,3 @@ public class Encryption {
         return decryptedText;
     }
 }
-
-//
-//public class AES_ENCRYPTION {
-//    private SecretKey key;
-//    private final int KEY_SIZE = 128;
-//    private final int DATA_LENGTH = 128;
-//    private Cipher encryptionCipher;
-//
-//    public void init() throws Exception {
-//        KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
-//        keyGenerator.init(KEY_SIZE);
-//        key = keyGenerator.generateKey();
-//    }
-//
-//    public String encrypt(String data) throws Exception {
-//        byte[] dataInBytes = data.getBytes();
-//        encryptionCipher = Cipher.getInstance("AES/GCM/NoPadding");
-//        encryptionCipher.init(Cipher.ENCRYPT_MODE, key);
-//        byte[] encryptedBytes = encryptionCipher.doFinal(dataInBytes);
-//
-//        return encode(encryptedBytes);
-//    }
-//
-//    public String decrypt(String encryptedData) throws Exception {
-//        byte[] dataInBytes = decode(encryptedData);
-//        Cipher decryptionCipher = Cipher.getInstance("AES/GCM/NoPadding");
-//        GCMParameterSpec spec = new GCMParameterSpec(DATA_LENGTH, decryptionCipher.getIV());
-//        decryptionCipher.init(Cipher.DECRYPT_MODE, key, spec);
-//        byte[] decryptedBytes = decryptionCipher.doFinal(dataInBytes);
-//
-//        return new String(decryptedBytes);
-//    }
-//
-//    private String encode(byte[] data) {
-//        return Base64.getEncoder().encodeToString(data);
-//    }
-//
-//    private byte[] decode(String data) {
-//        return Base64.getDecoder().decode(data);
-//    }
-//}
